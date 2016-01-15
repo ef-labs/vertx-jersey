4b670d19270af6793c8a289993c01cb51069e2da
Adrian Gonzalez <adrian.gonzalez@ef.com>
2016-01-15 13:29:41 -0500

    Bump swagger 1.5.6

--- examples/swagger/src/test/java/com/englishtown/vertx/jersey/examples/integration/SwaggerIntegrationTest.java
+++ examples/swagger/src/test/java/com/englishtown/vertx/jersey/examples/integration/SwaggerIntegrationTest.java
@@ -2,16 +2,29 @@ package com.englishtown.vertx.jersey.examples.integration;
 
 import com.englishtown.vertx.jersey.integration.JerseyHK2IntegrationTestBase;
 import com.englishtown.vertx.promises.RequestOptions;
+import io.swagger.jaxrs.listing.ApiListingResource;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.util.function.Consumer;
 
 public class SwaggerIntegrationTest extends JerseyHK2IntegrationTestBase {
 
     private String BASE_PATH = "http://localhost:8080/";
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Need to reset the static initialized flag
+        Field field = ApiListingResource.class.getDeclaredField("initialized");
+        field.setAccessible(true);
+        field.set(null, false);
+
+    }
 
     @Test
     public void testSwaggerJson() throws Exception {
